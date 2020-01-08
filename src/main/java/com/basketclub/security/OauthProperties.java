31b47103475dc0ac30f2a270a8ed3b0655e8b08b
andole87 <andole87@gmail.com>
2020-01-08 17:26:58 +0900

    feat(security): OauthProperties 추가

---
+++ src/main/java/com/basketclub/security/OauthProperties.java
@@ -0,0 +1,45 @@
+package com.basketclub.security;
+
+import lombok.Data;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Data
+@Component
+@PropertySource(value = "classpath:oauth.yml")
+public class OauthProperties implements InitializingBean {
+    @Value("${client.accessTokenUrl:#{null}}")
+    private String accessTokenUrl;
+
+    @Value("${client.clientId:#{null}}")
+    private String clientId;
+
+    @Value("${client.redirectUrl:#{null}}")
+    private String redirectUrl;
+
+    @Value("${client.resourceUrl:#{null}}")
+    private String resourceUrl;
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (hasNullField()) {
+            throw new BeanCreationException("oauth.yml 설정이 필요합니다");
+        }
+    }
+
+    private boolean hasNullField() {
+        return Arrays.stream(getClass().getDeclaredFields())
+                .anyMatch(field -> {
+                    try {
+                        return field.get(this) == null;
+                    } catch (IllegalAccessException e) {
+                        return true;
+                    }
+                });
+    }
+}
